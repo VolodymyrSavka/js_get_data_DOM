95777ead60cbfec48c0a1b732d134c8c776312ee
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-03 23:09:19 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const elements = document.getElementsByClassName('population');
+let sum = 0;
+let avarage = 0;
+
+for (const element of elements) {
+  const value = element.textContent;
+
+  const cleanedValue = value.replace(/[,\s\u00A0]/g, '');
+
+  if (!isNaN(Number(cleanedValue))) {
+    sum += Number(cleanedValue);
+  }
+}
+
+avarage = sum / elements.length;
+
+const calculateSum = document.querySelector('.total-population');
+
+calculateSum.textContent = sum.toLocaleString('uk-UA');
+
+const calcAv = document.querySelector('.average-population');
+
+calcAv.textContent = avarage.toLocaleString('uk-UA');
